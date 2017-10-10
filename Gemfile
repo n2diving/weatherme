a935166a8b8868c76244567e044b72bdc0dc2bf1
n2diving <alexis@gormley-design.com>
2017-10-10 02:35:40 -0700

    basic layout scaffolding, UI mixins, forecasts controller

--- Gemfile
+++ Gemfile
@@ -40,10 +40,14 @@ gem 'font-awesome-rails'
 gem 'geocoder'
 gem 'httparty'
 
+# DB
+gem 'annotate'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'dotenv-rails'
 end
 
 group :development do
---
+++ app/controllers/forecasts_controller.rb
@@ -0,0 +1,79 @@
+class ForecastsController < ApplicationController
+  before_action :set_forecast, only: [:show, :edit, :update, :destroy]
+
+  # GET /forecasts
+  # GET /forecasts.json
+  def index
+    @forecasts = Forecast.all
+  end
+
+  # GET /forecasts/1
+  # GET /forecasts/1.json
+  def show
+    city_id = params[:city_id]
+    app_id = ENV['OPEN_WEATHER_API_KEY']
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?id=#{city_id}&APPID=#{app_id}")
+
+    puts response.body, response.code, response.message, response.headers.inspect
+  end
+
+  # GET /forecasts/new
+  def new
+    @forecast = Forecast.new
+  end
+
+  # GET /forecasts/1/edit
+  def edit
+  end
+
+  # POST /forecasts
+  # POST /forecasts.json
+  def create
+    @forecast = Forecast.new(forecast_params)
+
+    respond_to do |format|
+      if @forecast.save
+        format.html { redirect_to @forecast, notice: 'Forecast was successfully created.' }
+        format.json { render :show, status: :created, location: @forecast }
+      else
+        format.html { render :new }
+        format.json { render json: @forecast.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /forecasts/1
+  # PATCH/PUT /forecasts/1.json
+  def update
+    respond_to do |format|
+      if @forecast.update(forecast_params)
+        format.html { redirect_to @forecast, notice: 'Forecast was successfully updated.' }
+        format.json { render :show, status: :ok, location: @forecast }
+      else
+        format.html { render :edit }
+        format.json { render json: @forecast.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /forecasts/1
+  # DELETE /forecasts/1.json
+  def destroy
+    @forecast.destroy
+    respond_to do |format|
+      format.html { redirect_to forecasts_url, notice: 'Forecast was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_forecast
+      @forecast = Forecast.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def forecast_params
+      params.fetch(:forecast, {})
+    end
+end
